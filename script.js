01c99c284fc258b2897e849db768d5f495a6e16c
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-28 17:10:37 +0530

    game

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var imgH = document.getElementById("imgH");
+  var imgC = document.getElementById("imgC");
+  var p1 = document.getElementById("p1");
+  var p2 = document.getElementById("p2");
+  var agreeBtn = document.getElementById("Agree");
+  var disagreeBtn = document.getElementById("disagree");
+  var mindChangeBtn = document.getElementById("mindChange");
+
+  imgH.style.display = "none";
+  imgC.style.display = "none";
+  p1.style.display = "none";
+  p2.style.display = "none";
+
+  // Hide the "Mind Change" button initially
+  mindChangeBtn.style.display = "none";
+
+  agreeBtn.addEventListener("click", function () {
+    imgH.style.display = "block";
+    p1.style.display = "block";
+    imgC.style.display = "none";
+    p2.style.display = "none";
+    showMindChangeButton();
+  });
+
+  disagreeBtn.addEventListener("click", function () {
+    imgC.style.display = "block";
+    p2.style.display = "block";
+    imgH.style.display = "none";
+    p1.style.display = "none";
+    showMindChangeButton();
+  });
+
+  mindChangeBtn.addEventListener("click", function () {
+    hideOptions();
+  });
+
+  function showMindChangeButton() {
+    mindChangeBtn.style.display = "inline-block";
+  }
+
+  function hideOptions() {
+    imgH.style.display = "none";
+    imgC.style.display = "none";
+    p1.style.display = "none";
+    p2.style.display = "none";
+    mindChangeBtn.style.display = "none";
+  }
+});
